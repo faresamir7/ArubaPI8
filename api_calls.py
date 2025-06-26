7f0c522ee16d39402a5d794fe7f5f0965a4983e9
Fares Amir Hassen <49094729+faresamir7@users.noreply.github.com>
2025-06-26 15:52:05 +0100

    Comment Update

--- api_calls.py
+++ api_calls.py
@@ -12,46 +12,47 @@ class ArubaAPIClient:
         self.password = password
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    def test_login(self):
-        url = f"https://{self.base_url}:4343/v1/api/login"
-        credentials = "username={}&password={}".format(self.username, self.password)
-        response = requests.post(url, verify=False, data=credentials)
-        if response.status_code == 200:
+    def test_login(self): ##Basic function to login
+        url = f"https://{self.base_url}:4343/v1/api/login" ##URL with IP or FQDN insert
+        credentials = "username={}&password={}".format(self.username, self.password) ##Credentials to send
+        response = requests.post(url, verify=False, data=credentials) ##Post request
+        if response.status_code == 200: ##Self test
+            print(json.dumps(response.json(), indent=2))
             token = response.json()['_global_result']['UIDARUBA']
             print("Login Successful, token is: {}".format(token))
             return token
         else:
             raise Exception(f"Login failed: {response.status_code}")
 
-    def login(self, session):
+    def login(self, session): ##New function to login
         headers = {}
         payload = ""
         cookies = ""
-        url = f"https://{self.base_url}:4343/v1/api/login"
-        loginparams = {'username': self.username, 'password' : self.password}
-        response = session.get(url, params=loginparams, headers=headers, data=payload, verify=False)
+        url = f"https://{self.base_url}:4343/v1/api/login" ##URL with IP or FQDN insert
+        loginparams = {'username': self.username, 'password' : self.password} ##Credentials to send
+        response = session.get(url, params=loginparams, headers=headers, data=payload, verify=False) ##Post request with empty Payload and Header
         if response.status_code == 200:
-            return response.json()['_global_result']['UIDARUBA']
+            return response.json()['_global_result']['UIDARUBA'] ##Returns UID
         else:
             raise Exception(f"Login failed: {response.status_code}")
 
-    def show_command(self, show_command: str):
-        session=requests.Session()
-        sessionToken=self.login(session)
+    def show_command(self, show_command: str): ##Show command function
+        session=requests.Session() ##Persistent session
+        sessionToken=self.login(session) ##Call to login function
         headers = {}
         payload = ""
         cookies = ""
         showParams = {
             'command' : show_command,
             'UIDARUBA' : sessionToken
-        }
-        response = session.get("https://"+self.base_url+":4343/v1/configuration/showcommand", params=showParams, headers=headers, data=payload, verify=False)
+        } ##Parameters containing UID for authentication and the show command
+        response = session.get("https://"+self.base_url+":4343/v1/configuration/showcommand", params=showParams, headers=headers, data=payload, verify=False) ##Get request with empty Payload
         if response.status_code == 200:
-            print(json.dumps(response.json(), indent=2))
+            print(json.dumps(response.json(), indent=2)) ##Prints the response
         else:
             raise Exception(f"Request failed: {response.status_code}")
 
-    def logout(self):
+    def logout(self): ##Logout function
         url = f"https://{self.base_url}:4343/v1/api/logout"
         response = requests.post(url, verify=False)
         if response.status_code != 200:
--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def login(base_url: str, username: str, password: str):
 def show_command(base_url: str, username: str, password: str, show_command: str):
     """Retrieve system information from the Aruba Mobility Controller."""
     client = ArubaAPIClient(base_url, username, password)
-    info = client.show_command(show_command)
-    typer.echo(info)
+    client.show_command(show_command)
     client.logout()
 
 if __name__ == "__main__":
