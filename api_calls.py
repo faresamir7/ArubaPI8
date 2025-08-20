c4e8b5a1042b47986bfeb062bc6c92272d03b14a
Fares Amir Hassen <49094729+faresamir7@users.noreply.github.com>
2025-08-20 11:49:45 +0100

    New function
    Added update function

--- api_calls.py
+++ api_calls.py
@@ -52,6 +52,21 @@ class ArubaAPIClient:
         else:
             raise Exception(f"Request failed: {response.status_code}")
 
+    def upgrade(self, filename: str, partition: str): ##Upgrade using TFTP
+        session=requests.Session() ##Persistent session
+        sessionToken=self.login(session) ##Call to login function
+        headers = {}
+        payload = '{"partition_num": "'+partition+'", "tftphost": "'+self.base_url+'", "filename": "'+filename+'"}'
+        cookies = ""
+        showParams = {
+            'UIDARUBA' : sessionToken
+        } ##Parameters containing UID for authentication and the show command
+        response = session.post("https://"+self.base_url+":4343/v1/configuration/object/copy_tftp_system", params=showParams, headers=headers, data=payload, verify=False) ##Get request with empty Payload
+        if response.status_code == 200:
+            print(json.dumps(response.json(), indent=2)) ##Prints the response
+        else:
+            raise Exception(f"Request failed: {response.status_code}")
+
     def logout(self): ##Logout function
         url = f"https://{self.base_url}:4343/v1/api/logout"
         response = requests.post(url, verify=False)
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 
 import typer
 from api_calls import ArubaAPIClient
+from typing_extensions import Annotated
 
 app = typer.Typer()
 
 
 @app.command()
-def login(base_url: str, username: str, password: str):
+def login(base_url: Annotated[str, typer.Argument()],
+         username: Annotated[str, typer.Argument()],
+          password: Annotated[str, typer.Argument()]):
     """Authenticate with the Aruba Mobility Controller."""
     client = ArubaAPIClient(base_url, username, password)
     client.test_login()
     client.logout()
 
 @app.command()
-def show_command(base_url: str, username: str, password: str, show_command: str):
+def show_command(base_url: Annotated[str, typer.Argument()],
+                username: Annotated[str, typer.Argument()],
+                password: Annotated[str, typer.Argument()],
+                show_command: Annotated[str, typer.Argument()]):
     """Retrieve system information from the Aruba Mobility Controller."""
     client = ArubaAPIClient(base_url, username, password)
     client.show_command(show_command)
+    client.logout()
+
+@app.command()
+def upgrade(base_url: Annotated[str, typer.Argument()],
+            username: Annotated[str, typer.Argument()], 
+            password: Annotated[str, typer.Argument()], 
+            filename: Annotated[str, typer.Argument()], 
+            partition: Annotated[str, typer.Argument()]):
+    """Upgrade Aruba Mobility Controller using TFTP."""
+    client = ArubaAPIClient(base_url, username, password)
+    client.upgrade(filename, partition)
     client.logout()
 
 if __name__ == "__main__":
